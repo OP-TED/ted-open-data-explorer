22cbc7cfd4ba949f00b0ed6041a13a84ed640a96
Cristian <cristianvasquez@gmail.com>
2025-01-23 15:07:36 +0100

    execute a query

--- src/app/App.vue
+++ src/app/App.vue
@@ -8,6 +8,11 @@ import rdf from 'rdf-ext'
 import { Parser } from 'n3'
 import SparqlEditor from './Editor.vue'
 
+// Define props for the SparqlEditor component
+const query = ref('') // This will hold the current query string
+const parseError = ref()
+const entities = ref()
+
 const focusOn = (type) => {
   return {
     ignoreNamedGraphs: true,
@@ -21,33 +26,66 @@ const focusOn = (type) => {
   }
 }
 
-const parseError = ref()
-const entities = ref()
 
+const executeQuery = async () => {
+  try {
+    const headers = new Headers({
+      'Accept': 'text/turtle',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    })
+
+    const response = await fetch('/sparql', {
+      method: 'POST',
+      headers: headers,
+      body: new URLSearchParams({ query: query.value }).toString()
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const responseData = await response.text()
+    const parser = new Parser({ format: 'text/turtle' })
+    const dataset = rdf.dataset([...parser.parse(responseData)])
+    const options = focusOn(ns.epo.Notice)
+    entities.value = getEntities(dataset, options)
+    parseError.value = null
+
+  } catch (error) {
+    parseError.value = error.message
+    entities.value = null
+  }
+}
 onMounted(() => {
+  // Initialize with some default query if needed
+  query.value = `PREFIX epo: <http://data.europa.eu/a4g/ontology#>
+PREFIX cccev: <http://data.europa.eu/m8g/>
 
-  const turtleStr = '<a> <b> <c> .'
-  const parser = new Parser()
-
-  const dataset = rdf.dataset([...parser.parse(turtleStr)])
-  const options = focusOn(ns.epo.Notice)
-  entities.value = getEntities(dataset, options)
+CONSTRUCT {
+  ?s ?p ?o
+}
+WHERE {
+  graph <http://data.europa.eu/a4g/resource/2023/533553_2023> {
+       ?s ?p ?o
+  }
+}`
 })
-
-
 </script>
 
 <template>
   <n-config-provider :theme="lightTheme">
-    <sparql-editor></sparql-editor>
-    {{ parseError }}
-    <div class="entity-container">
+    <n-space vertical>
+      <sparql-editor v-model="query"></sparql-editor>
+      <n-button @click="executeQuery">Execute Query</n-button>
+    </n-space>
+    <div v-if="parseError">{{ parseError }}</div>
+    <div class="entity-container" v-else>
       <EntityList :entities="entities"/>
     </div>
   </n-config-provider>
 </template>
-<style>
 
+<style>
 .entity-container {
   background-color: white;
 }
--- src/app/Editor.vue
+++ src/app/Editor.vue
@@ -1,55 +1,59 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+import { basicSetup } from 'codemirror'
+import { EditorView } from '@codemirror/view'
+import { EditorState } from '@codemirror/state'
+import { sparql } from 'codemirror-lang-sparql'
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const editorContainer = ref(null)
+const editorStyle = {
+  width: '100%',
+  height: '400px',
+  border: '1px solid #ddd',
+}
+
+let editor = null
+
+onMounted(() => {
+  editor = new EditorView({
+    state: EditorState.create({
+      doc: props.modelValue,
+      extensions: [
+        basicSetup,
+        sparql(),
+        EditorView.updateListener.of((update) => {
+          if (update.docChanged) {
+            emit('update:modelValue', update.state.doc.toString())
+          }
+        }),
+      ],
+    }),
+    parent: editorContainer.value,
+  })
+})
+
+// Watch for changes in the modelValue prop to update the editor's content
+watch(() => props.modelValue, (newValue) => {
+  if (editor && newValue !== editor.state.doc.toString()) {
+    editor.dispatch({
+      changes: {from: 0, to: editor.state.doc.length, insert: newValue}
+    })
+  }
+})
+</script>
+
 <template>
   <div ref="editorContainer" :style="editorStyle"></div>
 </template>
-
-<script>
-import { onMounted, ref } from 'vue';
-import { basicSetup } from 'codemirror';
-import { EditorView } from '@codemirror/view';
-import { EditorState } from '@codemirror/state';
-import { sparql } from 'codemirror-lang-sparql';
-
-export default {
-  name: 'SparqlEditor',
-  props: {
-    initialQuery: {
-      type: String,
-      default: 'SELECT * WHERE { ?s ?p ?o }' // default query
-    }
-  },
-  setup(props, { emit }) {
-    const editorContainer = ref(null);
-    const editorStyle = {
-      width: '100%',
-      height: '400px',
-      border: '1px solid #ddd',
-    };
-
-    onMounted(() => {
-      const editor = new EditorView({
-        state: EditorState.create({
-          doc: props.initialQuery,
-          extensions: [
-            basicSetup,
-            sparql(),
-            EditorView.updateListener.of((update) => {
-              if (update.docChanged) {
-                emit('update:query', update.state.doc.toString());
-              }
-            }),
-          ]
-        }),
-        parent: editorContainer.value
-      });
-    });
-
-    return {
-      editorContainer,
-      editorStyle
-    };
-  }
-};
-</script>
 
 <style scoped>
 .cm-editor {
