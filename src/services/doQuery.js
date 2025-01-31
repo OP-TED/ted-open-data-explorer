cc7c1b0738351378df332d1e6a0f4393216c8f60
Cristian <cristianvasquez@gmail.com>
2025-01-31 12:17:47 +0100

    add cellar endpoint

--- src/services/doQuery.js
+++ src/services/doQuery.js
@@ -1,24 +1,26 @@
-import { Parser } from "n3";
-import rdf from "rdf-ext";
+import { Parser } from 'n3'
+import rdf from 'rdf-ext'
 
-export async function executeQuery(query) {
+const sparqlEndpoint = import.meta.env.VITE_SPARQL_ENDPOINT
+
+export async function executeQuery (query) {
   const headers = new Headers({
-    Accept: "text/turtle",
-    "Content-Type": "application/x-www-form-urlencoded",
-  });
+    Accept: 'text/turtle',
+    'Content-Type': 'application/x-www-form-urlencoded',
+  })
 
-  const response = await fetch("/sparql", {
-    method: "POST",
+  const response = await fetch(sparqlEndpoint, {
+    method: 'POST',
     headers: headers,
     body: new URLSearchParams({ query }).toString(),
-  });
+  })
 
   if (!response.ok) {
-    const text = await response.text();
-    throw new Error(text);
+    const text = await response.text()
+    throw new Error(text)
   }
 
-  const responseData = await response.text();
-  const parser = new Parser({ format: "text/turtle" });
-  return rdf.dataset([...parser.parse(responseData)]);
+  const responseData = await response.text()
+  const parser = new Parser({ format: 'text/turtle' })
+  return rdf.dataset([...parser.parse(responseData)])
 }
