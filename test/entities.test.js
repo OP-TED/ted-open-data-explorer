791fd61299c5cf09b1b11ddbbaa1e931a6f260ea
Cristian <cristianvasquez@gmail.com>
2025-08-26 10:51:58 +0200

    update dependencies

--- test/entities.test.js
+++ test/entities.test.js
@@ -1,11 +1,8 @@
-import { expect } from 'expect'
-import toMatchSnapshot from 'expect-mocha-snapshot'
+import expect from 'expect'
 import { describe, it } from 'mocha'
 import { getRabbitDataset } from './support/dataset.js'
 import { getEntities } from '../src/traversers/entities.js'
 import rdf from 'rdf-ext'
-
-expect.extend({ toMatchSnapshot })
 
 const ns = {
   ex: rdf.namespace('http://example.org/'),
@@ -26,7 +23,7 @@ describe('bfs', () => {
       ],
     }
     const entities = getEntities(dataset, options)
-    expect(entities).toMatchSnapshot(this)
+    expect(entities).toMatchSnapshot()
   })
   it(`maxDepth = 2`, function () {
     const dataset = getRabbitDataset()
@@ -34,6 +31,6 @@ describe('bfs', () => {
       maxDepth: 2,
     }
     const entities = getEntities(dataset, options)
-    expect(entities).toMatchSnapshot(this)
+    expect(entities).toMatchSnapshot()
   })
 })
